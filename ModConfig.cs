6ed1c230d38fc288a909bb4743fc397321f372a0
shekurika <shekurika@gmail.com>
2021-02-27 15:27:44 +0100

    added config

---
+++ ModConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowthFix
+{
+    class ModConfig
+    {
+        public List<string> FixLocations { get; set; } = new List<string>() {
+                    "AguarCave",
+                    "RSVGreenhouse1",
+                    "RSVGreenhouse2",
+                    "DiannaGreenhouse"
+        };
+    }
+    
+}
--- ModEntry.cs
+++ ModEntry.cs
@@ -3,18 +3,25 @@ using StardewModdingAPI;
 using StardewValley;
 using Harmony;
 using StardewModdingAPI.Events;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GrowthFix
 {
     public class ModEntry : Mod
     {
+
+        private ModConfig Config;
         public override void Entry(IModHelper helper)
         {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+
             helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
             helper.Events.GameLoop.ReturnedToTitle += OnReturnedToTitle;
 
             HarmonyPatch.Monitor = Monitor;
+            HarmonyPatch.locations = Config.FixLocations;
             var harmony = HarmonyInstance.Create(this.ModManifest.UniqueID);
             HarmonyPatch.ApplyPatch(harmony);
         }
@@ -39,29 +46,31 @@ namespace GrowthFix
         {
             public static bool ApplyFixNextTick = true;
             public static IMonitor Monitor;
+            public static List<string> locations;
 
             public static void ApplyPatch(HarmonyInstance harmony)
             {
                 Monitor.Log("Prefixing SDVs method HoeDirt.dayUpdate");
                 harmony.Patch(
                     original: AccessTools.Method(typeof(StardewValley.TerrainFeatures.HoeDirt), nameof(StardewValley.TerrainFeatures.HoeDirt.dayUpdate), new[] { typeof(GameLocation), typeof(Microsoft.Xna.Framework.Vector2) }),
-                    prefix: new HarmonyMethod(typeof(HarmonyPatch), nameof(hoeDirt_dayUpdate_condSubstitute))
+                    prefix: new HarmonyMethod(typeof(HarmonyPatch), nameof(HoeDirt_dayUpdate_condSubstitute))
                 );
             }
 
-            private static bool hoeDirt_dayUpdate_condSubstitute(StardewValley.TerrainFeatures.HoeDirt __instance,  GameLocation environment, Microsoft.Xna.Framework.Vector2 tileLocation)
+            private static bool HoeDirt_dayUpdate_condSubstitute(StardewValley.TerrainFeatures.HoeDirt __instance,  GameLocation environment, Microsoft.Xna.Framework.Vector2 tileLocation)
             {
                 try
                 {
-                    if (ApplyFixNextTick)
+                    if (ApplyFixNextTick && locations.Contains(environment.name, StringComparer.OrdinalIgnoreCase))
                     {
                         HarmonyPatch.Monitor.Log("Prevented " + tileLocation.X + " " + tileLocation.Y + " in " + environment.name);
+                        return false;
                     }
-                    return !ApplyFixNextTick;
+                    return true;
                 }
                 catch (Exception ex)
                 {
-                    Monitor.Log($"Failed in {nameof(hoeDirt_dayUpdate_condSubstitute)}:\n{ex}", LogLevel.Error);
+                    Monitor.Log($"Failed in {nameof(HoeDirt_dayUpdate_condSubstitute)}:\n{ex}", LogLevel.Error);
                     return true;
                 }
             }
